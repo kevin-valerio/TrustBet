21b2cde8dc29f78c3d8235ddebe0c8eb3af61e72
Kevin Valerio <kevin.valerio@etu.univ-amu.fr>
2018-03-28 12:22:11 +0200

    Adding gradle

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -58,16 +58,16 @@ public final class R {
         public static final int center = 0x7f080029;
         public static final int dark = 0x7f080037;
         public static final int icon_only = 0x7f080055;
-        public static final int light = 0x7f080064;
-        public static final int none = 0x7f080076;
-        public static final int normal = 0x7f080077;
-        public static final int radio = 0x7f080085;
-        public static final int standard = 0x7f0800af;
-        public static final int text = 0x7f0800b5;
-        public static final int text2 = 0x7f0800b6;
-        public static final int toolbar = 0x7f0800c1;
-        public static final int wide = 0x7f0800cf;
-        public static final int wrap_content = 0x7f0800d2;
+        public static final int light = 0x7f080068;
+        public static final int none = 0x7f08007a;
+        public static final int normal = 0x7f08007b;
+        public static final int radio = 0x7f080089;
+        public static final int standard = 0x7f0800b3;
+        public static final int text = 0x7f0800b9;
+        public static final int text2 = 0x7f0800ba;
+        public static final int toolbar = 0x7f0800c6;
+        public static final int wide = 0x7f0800d4;
+        public static final int wrap_content = 0x7f0800d7;
     }
     public static final class integer {
         public static final int google_play_services_version = 0x7f090007;
--- app/src/main/java/salasca_valerio/trustbet/Database.java
+++ app/src/main/java/salasca_valerio/trustbet/Database.java
@@ -1,41 +1,10 @@
 package salasca_valerio.trustbet;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.target.BitmapImageViewTarget;
-import com.crashlytics.android.Crashlytics;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.Task;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
-import io.fabric.sdk.android.Fabric;
 public class Database {
 
     private static final String TAG = "JDBC_LOG";
